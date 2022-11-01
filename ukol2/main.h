b6ae9b6617805f5e6d8c331d733c4187a19b425d
Martin Čížek <martin.cizek@tul.cz>
2022-11-01 09:58:17 +0100

    Create main.h

---
+++ ukol2/main.h
@@ -0,0 +1,12 @@
+#define X1 -100
+#define X2 20
+#define X3 100
+#define Y1 2
+#define Y2 5
+#define Y3 8
+#define TEXT1 "Vlakno 1"
+#define TEXT2 "Vlakno 2"
+#define TEXT3 "Vlakno 3"
+#define T1 200ms
+#define T2 500ms
+#define T3 100ms
