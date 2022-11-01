84cb3ca9d5d4e13db2ecf9ffd51c12b5b210c4e6
Daniel Adámek <daniel.adamek123@icloud.com>
2022-11-01 10:09:16 +0100

    dokumentace k druhé uloze + readme

--- ukol2/main.cpp
+++ ukol2/main.cpp
@@ -2,6 +2,13 @@
 #include "main.h"
 #include "stm32746g_discovery_lcd.h"
 
+/*
+ * Support structure for displaying data on the display
+ * @param x number of pixels
+ * @param y line number
+ * @param text displayed text
+ * @param time sleep duration in milliseconds
+ */
 struct DispData {
     int x;
     int y;
@@ -9,11 +16,15 @@ struct DispData {
     std::chrono::milliseconds time;
 };
 
+/* Declaration of threads */
 Thread t1;
 Thread t2;
 Thread t3;
 
-
+/*
+ * @abstract Displays data on display with specific parameters
+ * @param DispData *data 	Data structure with properties
+ */
 void vypis(DispData *data) {
     while (1) {
         BSP_LCD_DisplayStringAt(data->x,LINE(data->y), data->text, CENTER_MODE);
@@ -34,32 +45,32 @@ int main()
     BSP_LCD_SetFont(&LCD_DEFAULT_FONT);
     BSP_LCD_SetBackColor(LCD_COLOR_WHITE);
     BSP_LCD_SetTextColor(LCD_COLOR_DARKBLUE);
-
+	
+    /* Declaration & Definition of data in first thread */
     DispData data1;
         data1.x = X1;
         data1.y = Y1;
         data1.text = (uint8_t*) TEXT1;
         data1.time = T1;
-
+    /* Declaration & Definition of data in second thread */
     DispData data2;
         data2.x = X2;
         data2.y = Y2;
         data2.text = (uint8_t*) TEXT2;
         data2.time = T2;
-
+    /* Declaration & Definition of data in third thread */
     DispData data3;
         data3.x = X3;
         data3.y = Y3;
         data3.text = (uint8_t*) TEXT3;
         data3.time = T3;
 
+    /* Start of threads */
     t1.start(callback(vypis, &data1));
     t2.start(callback(vypis, &data2));
     t3.start(callback(vypis, &data3));
 
     while (1) {
-        //t2.start(vypis2);
-        //t3.start(vypis3);
         HAL_Delay(2000);
     }
 }
--- ukol2/main.h
+++ ukol2/main.h
@@ -1,12 +1,19 @@
+/* X coords (pixels) */
 #define X1 -100
 #define X2 20
 #define X3 100
+
+/* Y coords (lines) */
 #define Y1 2
 #define Y2 5
 #define Y3 8
+
+/* Texts */
 #define TEXT1 "Vlakno 1"
 #define TEXT2 "Vlakno 2"
 #define TEXT3 "Vlakno 3"
+
+/* Sleep duration */
 #define T1 200ms
 #define T2 500ms
 #define T3 100ms
