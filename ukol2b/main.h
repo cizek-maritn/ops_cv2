ec68e554c2e7f3ba03b3eda22234720d722905bd
Daniel Adámek <oreoeescz@seznam.cz>
2022-11-15 10:06:03 +0100

    Update main.h

--- ukol2b/main.h
+++ ukol2b/main.h
@@ -1,18 +1,24 @@
+// X axes
 #define X1 -100
 #define X2 20
 #define X3 100
+// Y axes
 #define Y1 2
 #define Y2 5
 #define Y3 8
+// Texts
 #define TEXT1 "Vlakno 1"
 #define TEXT2 "Vlakno 2"
 #define TEXT3 "Vlakno 3"
+// Delays
 #define T1 600ms
 #define T2 500ms
 #define T3 300ms
+// Font colors
 #define C1 LCD_COLOR_RED
 #define C2 LCD_COLOR_GREEN
 #define C3 LCD_COLOR_BLUE
+// Background colors
 #define B1 LCD_COLOR_YELLOW
 #define B2 LCD_COLOR_MAGENTA
 #define B3 LCD_COLOR_BLACK
