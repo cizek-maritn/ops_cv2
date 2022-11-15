013234a8433989599d833d92dd02771f22e549c8
Martin Čížek <martin.cizek@tul.cz>
2022-11-15 09:46:45 +0100

    Create main.h

---
+++ ukol2b/main.h
@@ -0,0 +1,18 @@
+#define X1 -100
+#define X2 20
+#define X3 100
+#define Y1 2
+#define Y2 5
+#define Y3 8
+#define TEXT1 "Vlakno 1"
+#define TEXT2 "Vlakno 2"
+#define TEXT3 "Vlakno 3"
+#define T1 600ms
+#define T2 500ms
+#define T3 300ms
+#define C1 LCD_COLOR_RED
+#define C2 LCD_COLOR_GREEN
+#define C3 LCD_COLOR_BLUE
+#define B1 LCD_COLOR_YELLOW
+#define B2 LCD_COLOR_MAGENTA
+#define B3 LCD_COLOR_BLACK
