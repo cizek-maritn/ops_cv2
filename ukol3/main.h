3fd13585658d965bc6f38c7d171bb3bb5516e35f
Daniel Adámek <oreoeescz@seznam.cz>
2022-11-15 09:32:21 +0100

    ukol3 main.h

---
+++ ukol3/main.h
@@ -0,0 +1,18 @@
+/* X coords (pixels) */
+#define X1 -100
+#define X2 20
+#define X3 100
+
+/* Y coords (lines) */
+#define Y1 2
+#define Y2 5
+#define Y3 8
+
+/* Texts */
+#define TEXT1 "Vlakno 1"
+#define TEXT2 "Vlakno 2"
+#define TEXT3 "Vlakno 3"
+
+/* Sleep duration */
+#define T1 1000
+#define T2 2000
