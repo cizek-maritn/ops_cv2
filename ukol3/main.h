32af9f8e55c22ce67c7be60ef270e67e79411759
Daniel Adámek <oreoeescz@seznam.cz>
2022-11-29 10:24:14 +0100

    Create main.h

---
+++ ukol3/main.h
@@ -0,0 +1,40 @@
+#define SIZE_OF_PARKING 4
+
+/* X coords (pixels) */
+#define X1 0
+#define X2 0
+#define X3 0
+#define X4 0
+#define X5 0
+#define X6 0
+
+/* Texts */
+#define TEXT1 "Auto 1"
+#define TEXT2 "Auto 2"
+#define TEXT3 "Auto 3"
+#define TEXT4 "Auto 4"
+#define TEXT5 "Auto 5"
+#define TEXT6 "Auto 6"
+
+/* Sleep duration */
+#define T1 1000ms
+#define T2 1200ms
+#define T3 1500ms
+#define T4 1700ms
+#define T5 1900ms
+#define T6 2200ms
+// Font colors
+
+#define C1 LCD_COLOR_BLACK
+#define C2 LCD_COLOR_BLACK
+#define C3 LCD_COLOR_BLACK
+#define C4 LCD_COLOR_BLACK
+#define C5 LCD_COLOR_BLACK
+#define C6 LCD_COLOR_BLACK
+// Background colors
+#define B1 LCD_COLOR_YELLOW
+#define B2 LCD_COLOR_MAGENTA
+#define B3 LCD_COLOR_ORANGE
+#define B4 LCD_COLOR_RED
+#define B5 LCD_COLOR_GREEN
+#define B6 LCD_COLOR_BLUE
